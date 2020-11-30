e0ae2ce5032d804602a3e27c1ea6478b0208a048
Eleni Mandilara <madelinegr@gmail.com>
2020-12-01 00:17:12 +0100

    Add chart to display players' score

---
+++ src/components/Navigation.js
@@ -0,0 +1,72 @@
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import PersonIcon from '@material-ui/icons/Person';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Score from './Score';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function Navigation() {
+  const [value, setValue] = React.useState('recents');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <BottomNavigation value={value} onChange={handleChange}>
+        <BottomNavigationAction label="User" value="user" icon={<PersonIcon />} />
+        <BottomNavigationAction label="Score" value="score" onClick={handleOpen} icon={<EqualizerIcon />} />
+        <BottomNavigationAction label="Hall of fame" value="globalScore" icon={<AccountBalanceIcon />} />
+      </BottomNavigation>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <Score />
+          </div>
+        </Fade>
+      </Modal>
+    </Fragment>
+  );
+}
---
+++ src/components/Score.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Score of this game'
+  },
+  xAxis: {
+    categories: ['Jane', 'John', 'Joe'],
+    title: {
+      text: null
+    }
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Score'
+    },
+    labels: {
+      overflow: 'justify'
+    }
+  },
+  series: [{
+    type: 'column',
+    name: 'Jane',
+    data: [3, 2, 1]
+  }, {
+    type: 'column',
+    name: 'John',
+    data: [2, 3, 5]
+  }, {
+    type: 'column',
+    name: 'Joe',
+    data: [4, 3, 3]
+  }, {
+    type: 'spline',
+    name: 'Average',
+    data: [3, 2.67, 3]
+  }],
+  credits: {
+    enabled: false
+  }
+};
+
+export default function Score() {
+  return (<HighchartsReact highcharts={Highcharts} options={options} />)
+}
