e198fa8530b2a11408d78e3d3a2345e0b79b26f9
Eleni Mandilara <madelinegr@gmail.com>
2020-11-30 22:48:58 +0100

    Initial commit of game board and cards

---
+++ src/components/Board.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import GameCard from './GameCard';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  }
+}));
+
+export default function Board() {
+  const classes = useStyles();
+  const cards = [1, 2, 3, 4, 5, 6];
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        {cards.map(x =>
+          <Grid item xs={4}>
+            <GameCard foo={x} />
+          </Grid>
+        )}
+      </Grid>
+    </div>
+  );
+}
---
+++ src/components/GameCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(() => ({
+  gameCard: {
+    width: 240,
+    height: 350,
+  },
+  paper: {
+    textAlign: 'center',
+    width: 240
+  },
+}));
+
+export default function GameCard(props) {
+  const { foo } = { ...props };
+  const pictureUrl = `./resources/pictures/${foo}.jpg`;
+  
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.paper} elevation={10}>
+      <CardMedia
+        className={classes.gameCard}
+        image={ pictureUrl }
+      />
+    </Paper>
+
+    // <Card className={classes.gameCard}>
+    //   <CardMedia
+    //     className={classes.gameCard}
+    //     image={ pictureUrl }
+    //   />
+    // </Card>
+  );
+}
