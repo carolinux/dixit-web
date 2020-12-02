329c97aaa9f0f83549e2ee44435fae575555c921
Eleni Mandilara <madelinegr@gmail.com>
2020-12-02 20:18:52 +0100

    Create central component Board

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+}));
+
+export default function Board() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      {/* Board
+
+      [Score]  [Cards]
+
+      [Hand] */}
+    </div>
+  );
+}
